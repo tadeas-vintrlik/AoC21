5f4f787b3a45655500b054bf36810dd84c26fe19
tadeas-vintrlik <xvintr04@stud.fit.vutbr.cz>
2021-12-02 12:31:37 +0100

    format

--- day2/part1.go
+++ day2/part1.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type submarine struct {
-    horizontal, depth int
+	horizontal, depth int
 }
 
 func (s *submarine) forward(distance int) {
-    s.horizontal += distance
+	s.horizontal += distance
 }
 
 func (s *submarine) down(distance int) {
-    s.depth += distance
+	s.depth += distance
 }
 
 func (s *submarine) up(distance int) {
-    s.depth -= distance
+	s.depth -= distance
 }
 
 func main() {
-    s := submarine{depth: 0, horizontal: 0}
-    for {
-        var movement string
-        var distance int
-        n, _ := fmt.Scanf("%s %d", &movement, &distance)
-        if n == 0 {
-            break
-        }
-        switch movement {
-        case "forward":
-            s.forward(distance)
-            break
-        case "down":
-            s.down(distance)
-            break
-        case "up":
-            s.up(distance)
-            break
-        }
-    }
-    fmt.Println("Depth:", s.depth)
-    fmt.Println("Horizontal:", s.horizontal)
-    fmt.Println("Result:", s.depth * s.horizontal)
+	s := submarine{depth: 0, horizontal: 0}
+	for {
+		var movement string
+		var distance int
+		n, _ := fmt.Scanf("%s %d", &movement, &distance)
+		if n == 0 {
+			break
+		}
+		switch movement {
+		case "forward":
+			s.forward(distance)
+			break
+		case "down":
+			s.down(distance)
+			break
+		case "up":
+			s.up(distance)
+			break
+		}
+	}
+	fmt.Println("Depth:", s.depth)
+	fmt.Println("Horizontal:", s.horizontal)
+	fmt.Println("Result:", s.depth*s.horizontal)
 }
